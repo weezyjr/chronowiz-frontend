e3dadb7eab8024a1af305390e8db1abb78b22445
Mohamed Heiba <mohamed.adel.heiba@gmail.com>
2018-10-31 21:54:19 +0400

    Convert backend to HTTPS

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment =
   {
     production: false,
-    backendUrl: 'http://backend-dev.chronowiz.com:3000/',
+    backendUrl: 'https://backend-dev.chronowiz.com:3000/',
     s3MediaBucket: 'chronowiz-dev-media',
     S3MediaBucketUrl: 'https://s3-eu-west-1.amazonaws.com/chronowiz-dev-media/',
   };
