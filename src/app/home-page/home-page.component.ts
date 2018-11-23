f546bbbc719e5e7a15f545f71ec4cc1364a120ec
Mohamed Heiba <mohamed.adel.heiba@gmail.com>
2018-11-23 12:08:30 +0400

    Home page logos

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {Brand} from '../Brand/brand';
+import {BrandsService} from '../Brand/brands.service';
+import {ResponseData} from '../API/response-data';
+import {ResponseObject} from '../API/responseObject';
+import {NotificationsService} from 'angular2-notifications';
 
 @Component({
   selector: 'app-home-page',
@@ -8,12 +13,33 @@ import {Router} from '@angular/router';
 })
 export class HomePageComponent implements OnInit
 {
-  constructor(private router: Router)
+  brands: Brand[];
+
+  responseData: ResponseData;
+  response: ResponseObject;
+
+  constructor(private router: Router, private brandsService: BrandsService, private _notificationsService: NotificationsService)
   {
   }
 
   ngOnInit()
   {
+    this.brandsService.readAllBrands()
+      .subscribe(data =>
+      {
+        console.log(data);
+
+        this.responseData = data;
+        this.response = this.responseData.response;
+
+        if (this.response.type.match('ERROR'))
+        {
+          this._notificationsService.error('Error', this.response.message.en);
+        } else
+        {
+          this.brands = <Brand[]>this.response.payload;
+        }
+      });
   }
 
   userButtonClicked(): void
