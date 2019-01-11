5d27ec1384b579b2ea42afd3681f1d3443d559fa
Mohamed Heiba <mohamed.adel.heiba@gmail.com>
2019-01-11 17:44:55 +0400

    Temporarily replaced banner in the brands page with a white background

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment =
     backendUrl: 'http://localhost:3000/api/',
     s3MediaBucket: 'chronowiz-loc-media',
     S3MediaBucketUrl: 'https://s3-eu-west-1.amazonaws.com/chronowiz-loc-media/',
-    cloadFlareUrl: ' https://media.chronowiz.com/'
+    cloadFlareUrl: 'https://media.chronowiz.com/'
   };
 
 /*
