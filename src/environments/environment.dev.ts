6ad6918a7fe6679a0ac067dad9d283117c5ce4a2
Ahmed Adel <w33zy.7@gmail.com>
2020-02-22 13:48:37 +0200

    update environment

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment =
   {
     production: false,
-    backendUrl: 'https://backend-dev-alb.chronowiz.com/api/',
+    backendUrl: 'http://localhost:3000/api/',
     s3MediaBucket: 'chronowiz-dev-media',
     S3MediaBucketUrl: 'https://s3-eu-west-1.amazonaws.com/chronowiz-dev-media/',
     cloudfrontUrl: 'https://media-dev.chronowiz.com/'
